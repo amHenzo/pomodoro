42012d73b397d270f57d07e2a868d5869fe679b4
Enzo <en.lecoq2000@gmail.com>
2023-09-19 14:26:00 +0200

    :build: final-v2

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# pomodoro
+# pomodoro v1
+
 LE COQ enzo
+
+# Pomodoro
+
+Pomodoro est une application de gestion du temps basée sur la technique Pomodoro. Cette technique consiste à diviser son temps de travail en périodes de 25 minutes appelées "pomodoros", séparées par de courtes pauses. L'objectif est d'améliorer la productivité en évitant la fatigue et les distractions.
+
+## Fonctionnalités
+
+- Compte à rebours de 25 minutes pour chaque pomodoro
+- Pause automatique de 5 minutes entre chaque pomodoro
+
+## Installation
+
+1. Clonez le dépôt GitHub :
+
+```
+git clone https://github.com/amHenzo/pomodoro.git
+```
+
+2. ou éxécutez en ligne via git-hub pages
+
+## Utilisation
+
+1. Ouvrez l'application dans votre navigateur.
+3. Cliquez sur le bouton "play" pour commencer un pomodoro.
+4. Travaillez sans interruption jusqu'à ce que le compte à rebours atteigne zéro.
+5. Prenez une courte pause lorsque le pomodoro est terminé, d'une durée de base de 5 minutees
+6. le pomodoro reprends de 25 minutes en boucle
+
+
+
+
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -2,25 +2,33 @@ let button = document.querySelector("button")
 let resultmin = document.querySelector(".min")
 let resultsec = document.querySelector(".sec")
 let txtworking = document.querySelectorAll(".travail > p")
+let inputWork = document.querySelector('[name ="inputWork"]')
+let inputRes = document.querySelector('[name ="inputRes"]')
 
 // testing if we get the right <p> array
 //console.log(txtworking[0].textContent = "Teest")
 let isWorking = true;
-
+// default time in sec 
 let workTime =1500;
 let restTime =300;
 let idInterval =0;
-console.log(button)
 
 window.onload =(event)=>
 {
+    // if there is data saved load it 
+    if("saveWorkSec" in localStorage)
+    {
+        let saveWork = localStorage.getItem("saveWorkSec");
+        let saveRes = localStorage.getItem("saveResSec");
+
+        workTime = parseInt(saveWork);
+        RestTime = parseInt(saveRes);
+    }
     refreshtimer(workTime);
 }
 
 function start_timer()
 {
-    workTime = 1500;
-    restTime = 300;
 
     refreshtimer(workTime);
     // so it dosen't start mutiple chrono a the same time
@@ -58,9 +66,10 @@ function decrement()
             refreshtimer(restTime);
             restTime--;
         }
-    
     }
-    
+    // save time
+    localStorage.setItem("saveWorkSec",workTime);
+    localStorage.setItem("saveResSec",restTime);
     
 }
 
@@ -73,18 +82,36 @@ function refreshtimer(time)
 {
     if(isWorking)
     {
-        txtworking[0].style.color = 'green'
-        txtworking[1].style.color = 'black'
+        txtworking[0].style.color = 'limegreen'
+        txtworking[1].style.color = 'white'
     }
     else
     {
-        txtworking[0].style.color = 'black'
-        txtworking[1].style.color = 'green'
+        txtworking[0].style.color = 'white'
+        txtworking[1].style.color = 'limegreen'
     }
     let timemin = parseInt(time/60,10)  ;
     let timesec = parseInt(time % 60, 10);
+
+    //get a clock who always display atleast 4 digit 
     resultmin.textContent =  timemin < 10 ? "0"+ timemin:timemin;
     resultsec.textContent =  timesec < 10 ? "0"+ timesec:timesec;
 }
 
+/**
+ * know if inputs are goods
+ * 
+ * @param {time} d time who will get display
+ */
+function isInputok()
+{
+
+    inputWork.value = inputWork.value.replace(/[^\d]/,'');
+    inputRes.value = inputRes.value.replace(/[^\d]/,'');
+    
+    workTime = inputWork.value*60;
+    restTime = inputRes.value*60;
+    refreshtimer(workTime);
+    
+}
 
